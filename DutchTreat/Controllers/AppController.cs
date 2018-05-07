e8192a3f3224670b9bd9213479f1352fe0815d4b
samlee99 <sam1994lee@gmail.com>
2018-05-06 19:14:45 -0700

    First commit

---
+++ DutchTreat/Controllers/AppController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DutchTreat.Services;
+using DutchTreat.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DutchTreat.Controllers
+{
+    public class AppController : Controller
+    {
+        private IMailService _mailService;
+
+        public AppController(IMailService mailService)
+        {
+            _mailService = mailService;
+        }
+
+        public IActionResult Index()
+        {
+            //throw new InvalidOperationException("Bad things");
+
+            return View();
+        }
+
+        [HttpGet("contact")]
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [HttpPost("contact")]
+        public IActionResult Contact(ContactViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // send the email
+                _mailService.SendMessage("sam@sam.com", model.Subject, $"From: {model.Name} - {model.Email}, Message: {model.Message}.");
+                ViewBag.UserMessage = "Mail sent";
+                ModelState.Clear();
+            }
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewBag.Title = "About Us";
+
+            return View();
+        }
+    }
+}
---
+++ DutchTreat/wwwroot/js/index.js
@@ -0,0 +1,30 @@
+﻿$(document).ready(function () {
+    var x = 0;
+    var s = "";
+
+    console.log("Hello");
+
+
+
+    var theForm = $("#theForm");
+    theForm.hide();
+
+    var buyButton = $("#buyButton");
+    buyButton.on("click", function () {
+        console.log("Buying item");
+    });
+
+    var productInfo = $(".product-props li");
+    productInfo.on("click", function () {
+        console.log("You clicked on " + $(this).text());
+    });
+
+    var $loginToggle = $("#loginToggle");
+    var $popupForm = $(".popup-form");
+
+    $loginToggle.on("click", function () {
+        $popupForm.fadeToggle(1000);
+    });
+
+    
+});
